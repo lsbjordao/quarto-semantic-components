7ae333d701953eaec74df2f882552af15a00e25b
Lucas Sá Barreto Jordão <49884803+lsbjordao@users.noreply.github.com>
2026-09-18 01:22:53 -0300

    chore: align component dependency version with 0.1.0

--- _extensions/semantic-components/primitives.lua
+++ _extensions/semantic-components/primitives.lua
@@ -3,7 +3,7 @@ local function is_html() return FORMAT and FORMAT:match('html')~=nil end
 if quarto and quarto.doc and quarto.doc.add_html_dependency and is_html() then
   quarto.doc.add_html_dependency({
     name='quarto-semantic-components-primitives',
-    version='0.12.0',
+    version='0.1.0',
     stylesheets={'css/primitives.css'}
   })
 end
