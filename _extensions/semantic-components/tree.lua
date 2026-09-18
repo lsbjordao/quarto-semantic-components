c3abbdf3fdcbff73a5e18a499cbed77d8832fd4d
Lucas Sá Barreto Jordão <49884803+lsbjordao@users.noreply.github.com>
2026-09-18 01:22:44 -0300

    chore: align component dependency version with 0.1.0

--- _extensions/semantic-components/tree.lua
+++ _extensions/semantic-components/tree.lua
@@ -41,7 +41,7 @@ end
 if quarto and quarto.doc and quarto.doc.add_html_dependency and is_html() then
   quarto.doc.add_html_dependency({
     name='quarto-semantic-components-tree',
-    version='0.12.0',
+    version='0.1.0',
     scripts={'js/tree.js'},
     stylesheets={'css/tree.css'}
   })
