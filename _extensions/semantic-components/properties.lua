497a3e9281ede40c61f4f8bc52985cd9f4f93847
Lucas Sá Barreto Jordão <49884803+lsbjordao@users.noreply.github.com>
2026-09-18 01:22:47 -0300

    chore: align component dependency version with 0.1.0

--- _extensions/semantic-components/properties.lua
+++ _extensions/semantic-components/properties.lua
@@ -19,7 +19,7 @@ end
 if quarto and quarto.doc and quarto.doc.add_html_dependency and is_html() then
   quarto.doc.add_html_dependency({
     name='quarto-semantic-components-properties',
-    version='0.12.0',
+    version='0.1.0',
     stylesheets={'css/properties.css'}
   })
 end
